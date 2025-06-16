67c35855cc9b7c45f73a960da3e0a86e61c9ff86
Alex Estruch <93322160+aestruch13@users.noreply.github.com>
2025-06-16 10:51:45 +0000

    Añadí un boton que si hay tareas puedas eliminar todas de golpe

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -21,6 +21,11 @@ const Home = () => {
 		setTasks(newTasks);
 	}
 
+	//añado una funcion para poder eliminar todas las tareas solo clicando un boton, si no hay tareas el boton se ve apagado
+	const deleteAllTasks = () => {
+		setTasks([]);
+	}
+
 
 	return (
 		<div className="container my-5">
@@ -35,6 +40,15 @@ const Home = () => {
 					<p className="text-muted mt-3 text-end">
 						{tasks.length > 0 ? `${tasks.length} tareas pendientes` : "No hay tareas, para añadir escribe tu tarea y pulsa enter"}
 					</p>
+
+					<div className="text-end mt-3">
+						<button type="button" className={`btn ${tasks.length === 0 ? "btn-secondary" : "btn-danger"}`}
+							disabled={tasks.length === 0}
+							onClick={deleteAllTasks}
+						>
+							Eliminar todas las tareas
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
